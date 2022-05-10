e23bd993003d161b6819687b873c893084dd3d3c
Naomi Joy Smith <hmu@nextstepnaomi.com>
2022-05-10 18:21:49 +1200

    Pseudocode first pass

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -3,3 +3,40 @@
 // -    -   -   -   -  //
 
 console.log("Inflate The Unicorn!")
+// Define image tags as unicorns
+// Target click to unicorn
+// Work out which unicorn was clicked
+// Create an array of images
+// On click event calls function to replace image of unicorn
+// Image is replaced with the next in the array
+// Create function to read when horn is full of air
+// Create alert to thank mouse driver for their helium clicks if horn is full
+
+const unicorn = document.getElementsByTagName("img")
+let unicorns = (unicorn[0], unicorn[1], unicorn[2])
+//and what if we turn it into a string? thanks gerard
+
+//-Image Array-//
+
+
+// Write out a function to execute upon a later event
+function inflateHorn(e) {// responds to our imagetags (plural: see below for id)
+// Target click to unicorn
+let puff = 
+  //return array-loop called hornPuff
+  //check hornPuff index position{ }
+  responses(which unicorn?)
+  //assign index position {piff[i]=}  
+} // Target click to unicorn
+  // Work out which unicorn was clicked
+
+function responses(unicorn) {
+  //unicorns=
+  inflateHorn()
+  // call hornPuff index position with puff
+  //fetch image: [from-array]
+  puff(unicorn[i])
+  }
+  if image="position3"
+  alert("Tihei mauriora!")
+}
