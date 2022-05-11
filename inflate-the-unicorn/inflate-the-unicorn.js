f69afb5cb68e1596d6e3e6bfe1c530211993eabd
Naomi Joy Smith <hmu@nextstepnaomi.com>
2022-05-11 14:47:50 +1200

    Clarify pseudocode

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -3,20 +3,15 @@
 // -    -   -   -   -  //
 
 // -- PSEUDOCODE RUN-THROUGH -- //
-// Define image class as unicorns
+// Define image class as my pretty unicorns
 // Create an array of images identified as horns
 // Target click to unicorn
-// On click event calls function to replace image of unicorn (horn)
+// On click event calls function to replace image
 // Image is replaced with the next in the array
 // Create function to read when horn is full of air
 // Create alert to thank mouse driver for their helium clicks if horn is full
 
 const unicorn = document.getElementsByClassName('inflate-an-image')
-
-// function unicorns() {
-//   //and what if we turn unicorn positions into strings? thanks gerard
-//   return [unicorn[0], unicorn[1], unicorn[2]]
-// }
 
 //-Horn Image Array-//
 const horns = []
@@ -36,15 +31,16 @@ function inflateHorn(e) {
   console.log(puff)
   let horn = puff.src // Image index position is its horn
 
-  // call the function which reads the state of the unicorn
+  // image is redefined by the function which reads the state of the active unicorn and returns the next image
   puff.src = nextImage(horn)
 }
 
 function nextImage(horn) {
   console.log(horn)
-  // I could alternatively write a switch statement here and bring in unicorn-0 as our default...
+  // I could alternatively write a switch statement here...
   //fetch image: [from-array]
   if (horn.slice(-5) == horns[0].slice(-5)) {
+    // compare image number as workaround from inconsistent event target-driven image path
     return horns[1] // replace with next image
   } else if (horn.slice(-5) == horns[1].slice(-5)) {
     return horns[2]
