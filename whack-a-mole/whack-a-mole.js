454fc0e91afaa9296d25a91199f1d7692455890b
Naomi Joy Smith <hmu@nextstepnaomi.com>
2022-05-12 21:18:46 +1200

    Every time I whack that mole, that mole comes back for more

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -17,22 +17,19 @@ harry.id = 'mole'
 
 let randomIndex = whatHole() // running random function to generate a random number
 let randomHole = hole[randomIndex] // index corresponds to cells in the table
-// randomHole.appendChild(harry)
-// image becomes a property of the randomly generated hole
 
-function whatMole() {
-  randomHole.appendChild(harry)
-}
-whatMole()
+randomHole.appendChild(harry) // image becomes a property of the randomly generated hole
+
+const audio = new Audio()
+audio.src = 'whack-audio.wav'
 
 // onClick calls function
 // The function should remove the mole from the current cell and then, using a function that lets you re-use the code that you wrote earlier, add the mole to a new random cell.
-randomHole.onclick = function wHack(e) {
-  let hit = e.target
-  if (harry === hit) {
-    randomHole.removeChild(harry)
-    whatMole()
-  }
-  console.log(hit)
-  console.log(harry)
+harry.onclick = wHack
+
+function wHack() {
+  audio.play()
+  randomIndex = Math.floor(Math.random() * (25 - 0)) + 0
+  randomHole = hole[randomIndex]
+  randomHole.appendChild(harry)
 }
