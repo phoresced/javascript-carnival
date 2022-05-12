819c7c19e1af4579e6fdca5c86fd114cdcba82a9
Naomi Joy Smith <hmu@nextstepnaomi.com>
2022-05-12 17:35:13 +1200

    Can click a random mole, once

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -2,25 +2,37 @@
 // JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-let mole = './mole.png'
-console.log('line 5 ushers in this mischievous' + ' ' + mole)
-
 // - The ability to store or get references to the cells in the HTML table.
-let hole = document.getElementsByTagName('td')
-for (let i = 0; i < hole.length; i++) {
-  hole[i].onClick = popUp
-  console.log('line 12' + ' ' + hole[i])
-}
+const hole = document.getElementsByTagName('td')
 
 // - A function to randomly select which cell should show the mole.
-function popUp(e) {
-  let hole = e.target.random(0, 25)
-  hole.innerHTML.push(mole) // - A way to show a mole in the chosen cell.
+function whatHole() {
+  return Math.floor(Math.random() * (25 - 0)) + 0
 }
 
-// I'm defining each td tag as 'hole'
-// Then I run a for loop to create an array of holes
-// These holes each get an onClick even which calls the popUp function
-// I also need a way for the first mole to pop-up which could simply be a message 'click any square to begin'
-// The event function popUp tells whichever hole the array has produced to redefined itself as the target of the click, randomized within the range of available cells.
-// When the event is triggered the mole image is pushed into the innerHTML of a random hole.
+// - A way to show a mole in the chosen cell
+const harry = document.createElement('img')
+harry.src = './mole.PNG'
+harry.id = 'mole'
+
+let randomIndex = whatHole() // running random function to generate a random number
+let randomHole = hole[randomIndex] // index corresponds to cells in the table
+// randomHole.appendChild(harry)
+// image becomes a property of the randomly generated hole
+
+function whatMole() {
+  randomHole.appendChild(harry)
+}
+whatMole()
+
+// onClick calls function
+// The function should remove the mole from the current cell and then, using a function that lets you re-use the code that you wrote earlier, add the mole to a new random cell.
+randomHole.onclick = function wHack(e) {
+  let hit = e.target
+  if (harry === hit) {
+    randomHole.removeChild(harry)
+    whatMole()
+  }
+  console.log(hit)
+  console.log(harry)
+}
